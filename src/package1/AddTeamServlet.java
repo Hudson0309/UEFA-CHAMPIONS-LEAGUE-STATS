9000409b7d825ea39907e9786700ad0c9ab280bd
Hudson0309 <amitesh.saksham10@gmail.com>
2017-04-14 18:25:12 +0530

    AddTeamServlet Added

--- src/package1/AddTeamServlet.java
+++ src/package1/AddTeamServlet.java
@@ -2,6 +2,7 @@ package package1;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -28,6 +29,13 @@ public class AddTeamServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
+	    String tname=request.getParameter("tName");
+	    String tcountry=request.getParameter("tCountry");
+	    String tleague=request.getParameter("tLeague");
+	    teams tm=new teams();
+	    tm.addTeams(tname, tcountry, tleague);
+	
+	
 	}
 
 	/**
